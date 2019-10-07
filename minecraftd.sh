454aa741592cef142022bd0adc774e3609586972
Andrew <me@satook.com>
2019-10-07 20:42:16 +1100

    backup: Compression step is separate. Compression with xz takes a long time. Rather than keeping the server from for the entire duration, we instead take a quick (sub one second vs 5 mins for me) uncompressed backup and then compress with xz as a separate step. This will also allow for nicing the xz without extending the "no save" duration

--- minecraftd.sh
+++ minecraftd.sh
@@ -244,19 +244,22 @@ backup_files() {
 		exit 11
 	fi
 
-	FILE="$(date +%Y_%m_%d_%H.%M.%S).tar.xz"
+	FILE="$(date +%Y_%m_%d_%H.%M.%S).tar"
 	${SUDO_CMD} mkdir -p "${BACKUP_DEST}"
+	# do quick backup without compression
 	if server_is_running; then
 		echo -n "Starting running server backup to ${FILE}..."
 		game_command save-off
 		game_command save-all
 		sleep 10
-		${SUDO_CMD} tar -C "${SERVER_ROOT}" -cJf "${BACKUP_DEST}/${FILE}" --exclude '*/logs/*' .
+		${SUDO_CMD} tar -C "${SERVER_ROOT}" -cf "${BACKUP_DEST}/${FILE}" --exclude '*/logs/*' .
 		game_command save-on
 	else
 		echo -n "Starting offline server backup to ${FILE}..."
-		${SUDO_CMD} tar -C "${SERVER_ROOT}" -cJf "${BACKUP_DEST}/${FILE}" --exclude '*/logs/*' .
+		${SUDO_CMD} tar -C "${SERVER_ROOT}" -cf "${BACKUP_DEST}/${FILE}" --exclude '*/logs/*' .
 	fi
+	echo -n "compressing backup..."
+	${SUDO_CMD} xz -z "${BACKUP_DEST}/${FILE}"
 	echo -e "\e[39;1m done\e[0m\n"
 
 	echo -n "Only keeping the last ${KEEP_BACKUPS} backups and removing the other ones..."
